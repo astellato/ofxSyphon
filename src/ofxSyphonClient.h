b66cfa3e49e96a583aae6299b1f0ef3a48137c86
Tom Butterworth <bangnoise@gmail.com>
2025-03-28 20:54:43 +0000

    ofxSyphonClient add lockTexture()/unlockTexture() functions, deprecate bind()/unbind()
    decouple texture setup from binding so those who want to use the texture but not immediately bind it can do so

--- src/ofxSyphonClient.h
+++ src/ofxSyphonClient.h
@@ -26,25 +26,29 @@ class ofxSyphonClient {
     
     const std::string& getApplicationName();
     const std::string& getServerName();
-  
-    void bind();
-    void unbind();
-    
-    /*
-     To use the texture with getTexture()
-     you should surround it with bind() and 
-     unbind() functions */
-    
-    ofTexture& getTexture() {return mTex;}
     
     void draw(float x, float y, float w, float h);
     void draw(float x, float y);
     void drawSubsection(float x, float y, float w, float h, float sx, float sy, float sw, float sh);
     void drawSubsection(float x, float y, float sx, float sy, float sw, float sh);
 
-	
-	float getWidth();
-	float getHeight();
+    float getWidth();
+    float getHeight();
+
+    void lockTexture();
+    void unlockTexture();
+    
+    // calls lockTexture() then getTexture().bind()
+    OF_DEPRECATED_MSG("Use getTexture().bind()", void bind());
+    // calls getTexture().unbind() then unlockTexture()
+    OF_DEPRECATED_MSG("Use getTexture().unbind()", void unbind());
+    
+    /*
+     To use the texture with getTexture()
+     you must surround it with lockTexture() and
+     unlockTexture() functions */
+    
+    ofTexture& getTexture() {return mTex;}
     
 	protected:
 	ofxSyphonNSObject mClient;
